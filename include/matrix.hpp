a9861f6a22526ab7e91da21160f13d0b56aed66e
Jefffrey <tpigarelli@gmail.com>
2017-02-10 15:09:01 +0100

    Added matrix product
    Closed #9

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -20,4 +20,43 @@ namespace sor {
 
 	};
 
+	/* Implementation of the `sor::order` metaprogramming function.
+	*/
+	template<typename Type, std::size_t N, std::size_t M>
+	struct order<matrix<Type, N, M>>
+		: public std::integral_constant<std::size_t, 2> {};
+
+	/* Implementation of the `sor::extent` metaprogramming function.
+	*/
+	template<typename Type, std::size_t N, std::size_t M>
+	struct extent<matrix<Type, N, M>, 0>
+		: public std::integral_constant<std::size_t, N> {};
+
+	template<typename Type, std::size_t N, std::size_t M>
+	struct extent<matrix<Type, N, M>, 1>
+		: public std::integral_constant<std::size_t, M> {};
+
+	/* Implementation of the `std::is_tensor` metaprogramming function.
+	*/
+	template<typename Type, std::size_t N, std::size_t M>
+	struct is_tensor<matrix<Type, N, M>> : std::true_type {};
+
+	/* Matrix multiplication.
+	*/
+	template<typename LhsType, typename RhsType, std::size_t M, std::size_t N, std::size_t P>
+	auto operator*(matrix<LhsType, M, N>& lhs, matrix<RhsType, N, P> const& rhs) {
+		using common_type = typename std::common_type<LhsType, RhsType>::type;
+		using result_type = matrix<common_type, M, P>;
+		result_type result;
+		for (std::size_t m = 0; m < M; ++m) {
+			for (std::size_t p = 0; p < P; ++p) {
+				result(m, p) = common_type();
+				for (std::size_t n = 0; n < N; ++n) {
+					result(m, p) += (lhs(m, n) * rhs(n, p));
+				}
+			}
+		}
+		return result;
+	}
+
 }
--- test/sor/matrix.cpp
+++ test/sor/matrix.cpp
@@ -1,2 +1,94 @@
 #include "../../deps/catch/include/catch.hpp"
 #include "../../include/matrix.hpp"
+
+SCENARIO("matrix order query", "[matrix]") {
+
+	GIVEN("a matrix type") {
+
+		using matrix_type = sor::matrix<int, 1, 2>;
+
+		WHEN("we query the order value") {
+
+			constexpr auto order = sor::order<matrix_type>::value;
+
+			THEN("we get 2") {
+
+				REQUIRE(order == 2);
+
+			}
+
+		}
+
+	}
+
+}
+
+SCENARIO("matrix extent query", "[matrix]") {
+
+	GIVEN("a matrix type") {
+
+		using matrix_type = sor::matrix<int, 2, 3>;
+
+		WHEN("we query the extent values") {
+
+			constexpr auto extent1 = sor::extent<matrix_type, 0>::value;
+			constexpr auto extent2 = sor::extent<matrix_type, 1>::value;
+
+			THEN("we get the dimensionality of the matrix") {
+
+				REQUIRE(extent1 == 2);
+				REQUIRE(extent2 == 3);
+
+			}
+
+		}
+
+	}
+
+}
+
+SCENARIO("matrix multiplication", "[matrix]") {
+
+	GIVEN("two matrices") {
+
+		sor::matrix<int, 2, 3> matrix1({
+			1, 2, 3,
+			4, 5, 6
+		});
+
+		sor::matrix<long, 3, 4> matrix2({
+			1, 2, 3, 4,
+			5, 6, 7, 8,
+			9, 10, 11, 12
+		});
+
+		WHEN("we multiply them") {
+
+			auto result = matrix1 * matrix2;
+
+			THEN("the result is the matrix product") {
+
+				sor::matrix<long, 2, 4> expected({
+					38, 44, 50, 56,
+					83, 98, 113, 128,
+					128, 152, 176, 200
+				});
+				REQUIRE(result == expected);
+
+			}
+
+			THEN("the type is the common value type between the two matrices") {
+
+				constexpr bool is_long = std::is_same<
+					decltype(result)::value_type, 
+					long
+				>::value;
+				REQUIRE(is_long);
+
+			}
+
+		}
+
+	}
+
+}
